934e1903d4b3528b1eb576e2616fdb26f153241b
James Graham <thisisfordanjames@gmail.com>
2013-09-05 12:15:20 -0500

    encapsulated individual set card data

---
+++ Matchismo_again/Model/SetCard.h
@@ -0,0 +1,22 @@
+//
+//  SetCard.h
+//  Matchismo_again
+//
+//  Created by James Graham on 9/5/13.
+//  Copyright (c) 2013 James Graham. All rights reserved.
+//
+
+#import "Card.h"
+
+@interface SetCard : Card
+@property (strong, nonatomic) NSAttributedString *properties;
+@property (strong, nonatomic) NSString *symbol;
+@property (nonatomic, strong) NSNumber *dimension;
+@property (strong, nonatomic) UIColor *color;
+@property (strong, nonatomic) UIColor *shading;
++ (NSArray *)allPossibleDimensions;
++ (NSArray *)allPossibleSymbols;
++ (NSArray *)allPossibleShadings;
++ (NSArray *)allPossibleColors;
+
+@end
---
+++ Matchismo_again/Model/SetCard.m
@@ -0,0 +1,58 @@
+//
+//  SetCard.m
+//  Matchismo_again
+//
+//  Created by James Graham on 9/5/13.
+//  Copyright (c) 2013 James Graham. All rights reserved.
+//
+
+#import "SetCard.h"
+
+@implementation SetCard
+
++ (NSArray *)allPossibleColors{
+    return @[[UIColor greenColor], [UIColor purpleColor], [UIColor redColor]];
+}
+
++ (NSArray *)allPossibleRanks{
+    return @[[NSNumber numberWithInt:1], [NSNumber numberWithInt:2], [NSNumber numberWithInt:3]];
+}
+
++ (NSArray *)allPossibleShadings{
+    return [SetCard allPossibleColors];
+}
+
++ (NSArray *)allPossibleSymbols{
+    return @[@"▲",  @"●",  @"■"];
+}
+
+- (void)setSymbol:(NSString *)symbol{
+    if ([[SetCard allPossibleSymbols] containsObject:symbol]) {
+        self.symbol = symbol;
+    }
+}
+
+- (void)setDimension:(NSNumber *)dimension{
+    if([[SetCard allPossibleDimensions] containsObject:dimension]){
+        self.dimension = dimension;
+    }
+}
+
+- (void)setColor:(UIColor *)color{
+    if([[SetCard allPossibleColors] containsObject:color]) {
+        self.color = color;
+    }
+}
+
+- (void)setShading:(UIColor *)shading{
+    if([[SetCard allPossibleShadings] containsObject:shading]) {
+        self.shading = shading;
+    }
+}
+
+- (NSString *)contents{
+    return [NSString stringWithFormat:@"%@%@%@%@",self.symbol, self.dimension, self.color, self.shading ];
+    
+}
+
+@end
