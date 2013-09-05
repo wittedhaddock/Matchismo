eda619fd2d2e9911f502fab8a3fd6e7537ab9e61
James Graham <thisisfordanjames@gmail.com>
2013-09-05 11:14:07 -0500

    Multiple MVC - tab bar UI View Controller imp

---
+++ Matchismo_again/SetViewController.h
@@ -0,0 +1,13 @@
+//
+//  SetViewController.h
+//  Matchismo_again
+//
+//  Created by James Graham on 9/5/13.
+//  Copyright (c) 2013 James Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SetViewController : UIViewController
+
+@end
---
+++ Matchismo_again/SetViewController.m
@@ -0,0 +1,39 @@
+//
+//  SetViewController.m
+//  Matchismo_again
+//
+//  Created by James Graham on 9/5/13.
+//  Copyright (c) 2013 James Graham. All rights reserved.
+//
+
+#import "SetViewController.h"
+
+@interface SetViewController ()
+
+@end
+
+@implementation SetViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
